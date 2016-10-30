06d46863647fce34ba7841393cf2b50f7f09eb86
Dmitry Petrov <dpetroff@gmail.com>
2016-10-31 00:19:41 +0100

    basics of formula printing

--- src/main.lisp
+++ src/main.lisp
@@ -48,3 +48,44 @@
                      :root-system system
                      :missing-systems (remove-duplicates missing-systems :test #'string=)
                      :included-systems (remove-duplicates existing-systems)))))
+
+(defgeneric print-formula (formula))
+
+(defmethod print-formula ((formula <formula>))
+  (format nil "
+class ~a < Formula
+  desc ~S
+  homepage ~S
+  url ~S
+  sha256 ~S
+  head ~S
+"
+          (name formula)
+          (description formula)
+          (home-page formula)
+          (url formula)
+          (sha256 formula)
+          (repo-head formula)
+          ))
+
+(defun name (formula)
+  (asdf::component-name (root-system formula)))
+
+(defun description (formula)
+  (asdf::component-description (root-system formula)))
+
+(defun home-page (formula)
+    (declare (ignore formula))
+  "<home-page to be implemented>")
+
+(defun url (formula)
+  (declare (ignore formula))
+  "<url to be implemented>")
+
+(defun sha256 (formula)
+  (declare (ignore formula))
+  "<sha256 to be implemented>")
+
+(defun repo-head (formula)
+  (declare (ignore formula))
+  "<head to be implemented>")
